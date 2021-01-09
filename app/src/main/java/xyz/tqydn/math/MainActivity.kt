aabe39606556dc0bb4aa91190a378b6a683c431e
Taqikun021 <ataqiyudin21@gmail.com>
2021-01-09 16:40:03 +0700

    ui added

--- app/src/main/java/xyz/tqydn/math/MainActivity.kt
+++ app/src/main/java/xyz/tqydn/math/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_kalkulator, R.id.navigation_tes, R.id.navigation_materi, R.id.navigation_bank_soal, R.id.navigation_profil
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
